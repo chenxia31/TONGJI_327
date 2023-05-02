222bceb532fcc3028188874f376d64cea369ba44
chenxia <chenxia31@outlook.com>
2023-05-02 19:16:03 +0800

    modfied function write

--- detect.py
+++ detect.py
@@ -184,17 +184,25 @@ draw = time.time()
 
 
 def write(x, results):
+    """
+    x: tensor
+    x[1:5]:矩形框的四角
+    x[6]:
+    x[7]:类别代号
+
+    """
     c1 = tuple(x[1:3].int())
     c2 = tuple(x[3:5].int())
     img = results[int(x[0])]
     cls = int(x[-1])
     color = random.choice(colors)
-    label = "{0}".format(classes[cls])
-    cv2.rectangle(img, c1, c2,color, 1)
+    # 限制x[6].item()的小数点后面的位数
+    label = "{0}".format(classes[cls])+round(x[6].item(),3).__str__()
+    cv2.rectangle(img, (int(c1[0].item()), int(c1[1].item())), (int(c2[0].item()), int(c2[1].item())),color, 1)
     t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
     c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 4
-    cv2.rectangle(img, c1, c2,color, -1)
-    cv2.putText(img, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1);
+    cv2.rectangle(img, (int(c1[0].item()), int(c1[1].item())), (int(c2[0].item()), int(c2[1].item())),color, -1)
+    cv2.putText(img, label, (int(c1[0].item()), int(c1[1].item()) + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1);
     return img
 
 
--- video.py
+++ video.py
@@ -79,16 +79,15 @@ def write(x, results):
     cls = int(x[-1])
     color = random.choice(colors)
     label = "{0}".format(classes[cls])
-    cv2.rectangle(img, c1, c2,color, 1)
+    cv2.rectangle(img, (int(c1[0].item()), int(c1[1].item())), (int(c2[0].item()), int(c2[1].item())),color, 1)
     t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
     c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 4
-    cv2.rectangle(img, c1, c2,color, -1)
-    cv2.putText(img, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1);
+    cv2.rectangle(img, (int(c1[0].item()), int(c1[1].item())), (int(c2[0].item()), int(c2[1].item())),color, -1)
+    cv2.putText(img, label, (int(c1[0].item()), int(c1[1].item()) + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1);
     return img
 
 
 #Detection phase
-
 videofile = args.videofile #or path to the video file. 
 
 cap = cv2.VideoCapture(videofile)  
